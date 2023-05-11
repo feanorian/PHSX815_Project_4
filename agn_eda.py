cc86d89df5f8fb24337ee12cf7590c88770ab2b5
Craig L Brooks <feanorian@users.noreply.github.com>
2023-05-11 11:35:03 -0500

    Add files via upload

---
+++ agn_eda.py
@@ -0,0 +1,108 @@
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+import pandas as pd
+import seaborn as sns
+sys.path.append(".")
+import scipy.stats as st
+from scipy.stats import bootstrap
+
+if __name__ == "__main__":
+
+	if '-h' in sys.argv or '--help' in sys.argv:
+		print ("Usage: %s [-t -n]" % sys.argv[0])
+		print
+		sys.exit(1)
+	if '-f' in sys.argv:
+		p = sys.argv.index('-f')
+		InputFile = sys.argv[p+1]
+	else:
+		InputFile = 'mbh.csv'
+	if '-mass' in sys.argv:
+		p = sys.argv.index('-mass')
+		mass = sys.argv[p+1]
+	else:
+		mass = '7'
+
+	np.random.seed(534)
+	rng = np.random.default_rng()
+
+	InputFile = 'mbh.csv'
+	with open(InputFile) as file:
+		df=pd.read_csv(file)
+
+	# removes entries for which we have no mass data
+	clean_df=df[df['log M_BH'] != 'NAN']
+
+	# converts the mass column to numerical data
+	clean_df['log M_BH'] = pd.to_numeric(clean_df['log M_BH'])
+	clean_df['log Z'] = np.log(clean_df['Redshift'])
+
+	# Cut of AGN data by mass
+	small_bh = clean_df[clean_df['log M_BH'] <= float(mass)]
+	large_bh = clean_df[clean_df['log M_BH'] > float(mass)]
+
+	# clean redshift data
+	data1 = (small_bh['log Z'],)
+	data2 = (large_bh['log Z'],)
+
+	# Bootstrap of stdev of log(Z) for small and large mass AGN
+	small_boot_std_z = bootstrap(data1, np.std, confidence_level=0.95,random_state=rng)
+	large_boot_std_z = bootstrap(data2, np.std, confidence_level=0.95,random_state=rng)
+	
+	# Bootstrap om mean of log(Z) for small and large mass AGN
+	small_boot_mean_z = bootstrap(data1, np.mean, confidence_level=0.95,random_state=rng)
+	large_boot_mean_z = bootstrap(data2, np.mean, confidence_level=0.95,random_state=rng)
+
+	small_boot_std_z_data = small_boot_std_z.bootstrap_distribution
+	small_boot_mean_z_data = small_boot_mean_z.bootstrap_distribution
+	large_boot_std_z_data = large_boot_std_z.bootstrap_distribution
+	large_boot_mean_z_data = large_boot_mean_z.bootstrap_distribution
+
+
+	# plot regression lines for total data
+	ax = plt.axes()
+	sns.regplot(x = 'log Z',y = 'log M_BH',data=clean_df, line_kws = {'color':'red'})
+	plt.title('AGN log(M_BH) vs log(z)')
+	plt.xlabel('Redshift (log(z))')
+	plt.savefig('total_reg')
+	plt.show()
+
+	# plot regression lines for large mass AGN
+	ax1 = plt.axes()
+	sns.regplot(x='log Z',y='log M_BH', data = large_bh,line_kws = {'color':'red'})
+	plt.title('log(M_BH) vs log(z) for large AGN')
+	plt.xlabel('Redshift (log(z))')
+	plt.savefig('large_reg')
+	plt.show()
+
+	# plot regression lines for small mass AGN
+	ax2 = plt.axes()
+	sns.regplot(x='log Z',y='log M_BH', data = small_bh,line_kws = {'color':'red'})
+	plt.title('log(M_BH) vs log(z) for small AGN')
+	plt.xlabel('Redshift (log(z))')
+	plt.savefig('small_reg')
+	plt.show()
+
+	# histplot for mean of STD of log(Z)
+	fig, ax = plt.subplots()
+	sns.histplot(small_boot_std_z_data, stat='probability',bins=25, color = 'salmon', element="step",fill=True, alpha=.3, label=rf'$M_{{BH}} < 10^{{{mass}}} M_{{Sun}}$')
+	sns.histplot(large_boot_std_z_data, bins=25, stat = 'probability',color = 'aqua', element="step",fill=True, alpha=.3, label=rf'$M_{{BH}} > 10^{{{mass}}} M_{{Sun}}$')
+	ax.set_title(r'Bootstrap Distribution for $\sigma_{log(Z)}$ AGN M_BH')
+	ax.set_xlabel('standard deviation')
+	ax.set_ylabel('probability')
+	plt.legend()
+	plt.savefig('hist_sigma_boot')
+	plt.show()
+
+	# Histplot for mean of log(Z)
+	fig1, ax1 = plt.subplots()
+	sns.histplot(small_boot_mean_z_data, stat='probability',bins=25, color = 'salmon', element="step",fill=True, alpha=.3, label=rf'$M_{{BH}} < 10^{{{mass}}} M_{{Sun}}$')
+	sns.histplot(large_boot_mean_z_data, bins=25, stat = 'probability',color = 'aqua', element="step",fill=True, alpha=.3, label=rf'$M_{{BH}} > 10^{{{mass}}} M_{{Sun}}$')
+	ax1.set_title('Bootstrap Distribution for log(Z) based on AGN M_BH')
+	ax1.set_xlabel('mean log (Z)')
+	ax1.set_ylabel('probability')
+	plt.legend()
+	plt.savefig('hist_logz_boot')
+	plt.show()
---
+++ ll_analysis.py
@@ -0,0 +1,105 @@
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+import pandas as pd
+import seaborn as sns
+from scipy.stats import bootstrap
+import scipy.stats as st
+
+if __name__ == "__main__":
+
+
+	if '-h' in sys.argv or '--help' in sys.argv:
+		print ("Usage: %s [-t -n]" % sys.argv[0])
+		print
+		sys.exit(1)
+	if '-f' in sys.argv:
+		p = sys.argv.index('-f')
+		InputFile = sys.argv[p+1]
+	else:
+		InputFile = 'mbh.csv'
+	if '-mass' in sys.argv:
+		p = sys.argv.index('-mass')
+		mass = float(sys.argv[p+1])
+	else:
+		mass = 7
+
+
+	np.random.seed(534)
+	rng = np.random.default_rng()
+	
+	def find_nearest(array, value):
+		array = np.asarray(array)
+		idx = (np.abs(array - value)).argmin()
+		return array[idx]
+	
+	#InputFile = 'mbh.csv'
+	with open(InputFile) as file:
+		df=pd.read_csv(file)
+
+	# removes entries for which we have no mass data
+	clean_df=df[df['log M_BH'] != 'NAN']
+
+	# converts the mass column to numerical data
+	clean_df['log M_BH'] = pd.to_numeric(clean_df['log M_BH'])
+	clean_df['log Z'] = np.log(clean_df['Redshift'])
+
+	# Cut of AGN data by mass
+
+	small_bh = clean_df[clean_df['log M_BH'] <= mass]
+	large_bh = clean_df[clean_df['log M_BH'] > mass]
+
+	data1 = (large_bh['log Z'],)
+	data2 = (small_bh['log Z'],)
+	
+
+	# bootstrap sample for the data
+	small_boot_std_z = bootstrap(data1, np.std, confidence_level=0.95,random_state=rng)
+	large_boot_std_z = bootstrap(data2, np.std, confidence_level=0.95,random_state=rng)
+	small_boot_mean_z = bootstrap(data1, np.mean, confidence_level=0.95,random_state=rng)
+	large_boot_mean_z = bootstrap(data2, np.mean, confidence_level=0.95,random_state=rng)
+
+	# log(Z) and stdev for small and large mass populations computed from bootstrap
+	small_boot_std_z_data = small_boot_std_z.bootstrap_distribution
+	small_boot_mean_z_data = small_boot_mean_z.bootstrap_distribution
+	large_boot_std_z_data = large_boot_std_z.bootstrap_distribution
+	large_boot_mean_z_data = large_boot_mean_z.bootstrap_distribution
+
+	small_mean_z_true = np.mean(small_boot_mean_z_data)
+	small_sigma_true = np.mean(small_boot_std_z_data)
+	large_mean_z_true = np.mean(large_boot_mean_z_data)
+	large_sigma_true = np.mean(large_boot_std_z_data)
+	# Range of redshifts
+	x = np.linspace(-7, 1, 10000, endpoint=True)
+
+	likelihood_H0 = [st.norm.pdf(data1,i,large_sigma_true).sum() for i in x]
+	likelihood_H1 = [st.norm.pdf(data2,i,large_sigma_true).sum() for i in x]
+
+	likelihood_H0 = np.asarray(likelihood_H0)
+	likelihood_H1 = np.asarray(likelihood_H1)
+	likelihoodr_H0 = np.log10(likelihood_H0/likelihood_H1)
+	likelihoodr_H1 = np.log10(likelihood_H1/likelihood_H0)
+
+	crit = list(likelihoodr_H0).index(find_nearest(likelihoodr_H0, value=0.0))
+	reject = round(find_nearest(likelihoodr_H0, value=0.0), 4)
+	beta = (likelihoodr_H0 == likelihoodr_H0[crit]).sum()  / len(likelihoodr_H0)
+	# confidence interval
+	S_95 = small_boot_mean_z.confidence_interval
+	L_95 = large_boot_mean_z.confidence_interval
+
+
+	textstr = '\n'.join((
+    	rf'$\beta$ = {round(beta, 4)}',
+    	rf'power = {round((1-beta), 4)}'))
+
+	sns.histplot(likelihoodr_H0, stat='probability',bins=50, element="step",fill=True, alpha=.3 ,color = 'aqua' , label = rf'$P(log_{{M_{{BH}}}} \leq {mass} | H0$)')
+	sns.histplot(likelihoodr_H1, stat='probability',bins=50, element="step",fill=True, alpha=.3,color = 'salmon', label = rf'$P(log_{{M_{{BH}}}} \leq {mass} | H1$)')
+	plt.title('Log Likelihood Ratios for H0 and H1')
+	plt.xlabel('log (L(H0)/L(H1))')
+	plt.yscale('log')
+	plt.axvline(x = find_nearest(likelihoodr_H0, value=0.0), color='r', label=rf'LH0 = LH1')
+	plt.annotate(textstr, xy=(0.05, 0.65), xycoords='axes fraction')
+	plt.legend()
+	plt.savefig('LLR')
+	plt.show()
